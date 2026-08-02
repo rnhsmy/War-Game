66f3e7f41a8f6ffcf94842d0dbec8d2a47faebbe
Ryan Massey <massryan@sheridancollege.ca>
2026-08-01 23:04:30 -0400

    Fixing errors

--- src/ca/sheridancollege/project/GroupOfCards.java
+++ src/ca/sheridancollege/project/GroupOfCards.java
@@ -1,6 +1,11 @@
 /**
  * Students can modify and extend to implement their game.
  * Add your name as a modifier and the date!
+ *
+ * Modified by Ryan, 2026: initialized the cards list in the constructor
+ * (it was previously left null, which would cause a NullPointerException
+ * the first time shuffle() or showCards() was used), and added the basic
+ * add/remove helper methods a card game needs to move cards between groups.
  */
 package ca.sheridancollege.project;
 
@@ -23,6 +28,7 @@ public class GroupOfCards
     public GroupOfCards(int givenSize)
     {
         size = givenSize;
+        cards = new ArrayList<>();
     }
     
     /**
@@ -51,6 +57,58 @@ public class GroupOfCards
      */
     public void setSize(int givenSize) {
         size = givenSize;
+    }
+    
+    // --- The methods below were added by Ryan, 2026, so this class can
+    //     actually be used to deal, draw, and win cards during play. ---
+
+    /**
+     * Adds a single card to the bottom of this group.
+     * @param card the card to add
+     */
+    public void addCard(Card card)
+    {
+        cards.add(card);
+    }
+
+    /**
+     * Adds a whole collection of cards to the bottom of this group, in order.
+     * Handy for a player collecting a won pot, for example.
+     * @param cardsToAdd the cards to add
+     */
+    public void addCards(ArrayList<Card> cardsToAdd)
+    {
+        cards.addAll(cardsToAdd);
+    }
+
+    /**
+     * Removes and returns the top card (index 0) of this group.
+     * @return the top card, or null if this group is empty
+     */
+    public Card removeCard()
+    {
+        if (cards.isEmpty())
+        {
+            return null;
+        }
+        return cards.remove(0);
+    }
+
+    /**
+     * @return true if this group currently has no cards in it
+     */
+    public boolean isEmpty()
+    {
+        return cards.isEmpty();
+    }
+
+    /**
+     * @return how many cards are currently in this group (distinct from
+     *         getSize(), which is just the configured maximum/target size)
+     */
+    public int numCards()
+    {
+        return cards.size();
     }
     
 }//end class
